5c78108cca493ba1b46d660677dbc350db4bed70
Caitlin Winters <caitlin.p.winters@gmail.com>
2021-08-05 16:21:54 -0400

    Display functionality implemented

--- calculator.js
+++ calculator.js
@@ -1,28 +1,56 @@
-// //Write a functions for : add, subtract, multiple and divide
-// debugger;
-// var add = function (num1, num2) {
-//   return num1 + num2;
-// };
+//Create a variable to hold the first number that is pressed
 
-// var subtract = function (num1, num2) {
-//   return num1 - num2;
-// };
+var currentNum
+var output = currentNum
 
-// var multiple = function(num1, num2) {
-//   return num1 * num2
-// };
 
-// var divide = function(num1, num2) {
-//   return num1 / num2
-// };
+//Display the clicked number
+var display = document.getElementById("display");
+var numOne = document.getElementById("one")
 
-// // console.log(add(1, 2)) //3
 
-// var calculate = function (func, num1, num2) {
-//   return func(num1, num2)
-// };
-// // console.log(calculate(add, 1, 2)); //3
+numOne.onclick = function () {
+  //need to find out how to make it the id of the button clicked
+  display.innerHTML = numOne.value
+  currentNum = numOne.value
+}
 
-var numOne = document.getElementsByClassName('one');
-console.log(numOne)
-console.log('test')
+// Clear the output and display
+var clear = document.getElementById("clear")
+clear.onclick =  function () {
+  display.innerHTML = ''
+}
+
+
+//Display needs to show the last element clicked
+var display
+
+//When the "equals" button i
+// onClick
+// var operator
+
+//Write a functions for : add, subtract, multiple and divide
+var add = function (num1, num2) {
+  return num1 + num2;
+};
+
+var subtract = function (num1, num2) {
+  return num1 - num2;
+};
+
+var multiple = function(num1, num2) {
+  return num1 * num2
+};
+
+var divide = function(num1, num2) {
+  return num1 / num2
+};
+
+// console.log(add(1, 2)) //3
+
+var calculate = function (func, num1, num2) {
+  return func(num1, num2)
+};
+// console.log(calculate(add, 1, 2)); //3
+
+
