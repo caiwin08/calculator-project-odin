67fd1c91d918970f73d0d53b13bc300ba5fe2643
Caitlin Winters <caitlin.p.winters@gmail.com>
2021-08-05 12:12:23 -0400

    Added calculator framework in html

--- calculator.js
+++ calculator.js
@@ -1,24 +1,28 @@
-//Write a functions for : add, subtract, multiple and divide
-debugger;
-var add = function (num1, num2) {
-  return num1 + num2;
-};
+// //Write a functions for : add, subtract, multiple and divide
+// debugger;
+// var add = function (num1, num2) {
+//   return num1 + num2;
+// };
 
-var subtract = function (num1, num2) {
-  return num1 - num2;
-};
+// var subtract = function (num1, num2) {
+//   return num1 - num2;
+// };
 
-var multiple = function(num1, num2) {
-  return num1 * num2
-};
+// var multiple = function(num1, num2) {
+//   return num1 * num2
+// };
 
-var divide = function(num1, num2) {
-  return num1 / num2
-};
+// var divide = function(num1, num2) {
+//   return num1 / num2
+// };
 
-// console.log(add(1, 2)) //3
+// // console.log(add(1, 2)) //3
 
-var calculate = function (func, num1, num2) {
-  return func(num1, num2)
-};
-// console.log(calculate(add, 1, 2)); //3
+// var calculate = function (func, num1, num2) {
+//   return func(num1, num2)
+// };
+// // console.log(calculate(add, 1, 2)); //3
+
+var numOne = document.getElementsByClassName('one');
+console.log(numOne)
+console.log('test')
